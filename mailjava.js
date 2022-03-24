d31a16b1eece958f5ae5e75f6afdbb49e0761974
Gennaro <gennarinodi02@gmail.com>
2022-03-24 15:43:37 +0100

    mail+bonus

--- mailjava.js
+++ mailjava.js
@@ -1,11 +1,33 @@
 
 let email = prompt("inserisci l'email");
 let lista =["matteo@gmail.com","fabio@gmail.com","pietro@gmail.com","anna@gmail.com"];
+let controllo=0;
+let richiesta=0;
 
 for(i=0; i<=lista.length;i++)
 {
- if(email==lista[i])
-        alert("puoi accedere");
-        else
-        alert("non puoi accedere")
+ if(email == lista[i])
+ {
+    alert(" puoi accedere");
+    controllo++;
+    break;   
+ }
+
+}
+
+
+if(controllo==0)
+
+{
+    alert("non puoi accedere ");
+    richiesta = prompt("vuoi inserire questa email nella lista ? premi 's' per inserire 'n'");
+    if (richiesta=="s")
+    {
+        lista.push(email);
+    }
+    else
+    {
+        alert("ok");
+    }
+console.log(lista)
 }
