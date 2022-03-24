ec696f52f36c911a51a83a8682b56095d43514fa
Gennaro <gennarinodi02@gmail.com>
2022-03-24 14:20:48 +0100

    1checkpoint

---
+++ mailjava.js
@@ -0,0 +1,11 @@
+
+let email = prompt("inserisci l'email");
+let lista =["matteo@gmail.com","fabio@gmail.com","pietro@gmail.com","anna@gmail.com"];
+
+for(i=0; i<=lista.length;i++)
+{
+ if(email==lista[i])
+        alert("puoi accedere");
+        else
+        alert("non puoi accedere")
+}
